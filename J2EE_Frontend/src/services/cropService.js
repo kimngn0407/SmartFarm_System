fff71197598bd9994bd9be2e6b4a91d4eb4feac2
kimngn0407 <lovengan0407@gmail.com>
2025-11-08 11:54:01 +0700

    Fix: Sửa relative URLs trong cropService - Dùng API_BASE_URL để gọi đúng backend

--- J2EE_Frontend/src/services/cropService.js
+++ J2EE_Frontend/src/services/cropService.js
@@ -5,18 +5,19 @@ const getAuthHeader = () => {
     const token = localStorage.getItem('token');
     return token ? { Authorization: `Bearer ${token}` } : {};
 };
-const getCropsByField = () => axios.get(`/api/plants`, { headers: getAuthHeader() });
-const getCropById = (id) => axios.get(`/api/crops/${id}`, { headers: getAuthHeader() });
+// Tất cả API calls phải dùng API_BASE_URL để gọi đúng backend
+const getCropsByField = () => axios.get(`${API_BASE_URL}/api/plants`, { headers: getAuthHeader() });
+const getCropById = (id) => axios.get(`${API_BASE_URL}/api/crops/${id}`, { headers: getAuthHeader() });
 const createCrop = (data) => axios.post(`${API_BASE_URL}/api/crops`, data, { headers: getAuthHeader() });
-const updateCrop = (id, data) => axios.put(`/api/crops/${id}`, data, { headers: getAuthHeader() });
-const deleteCrop = (id) => axios.delete(`/api/crops/${id}`, { headers: getAuthHeader() });
-const getFlatStagesByPlantId = (plantId) => axios.get(`/api/plants/${plantId}/flat-stages`, { headers: getAuthHeader() });
+const updateCrop = (id, data) => axios.put(`${API_BASE_URL}/api/crops/${id}`, data, { headers: getAuthHeader() });
+const deleteCrop = (id) => axios.delete(`${API_BASE_URL}/api/crops/${id}`, { headers: getAuthHeader() });
+const getFlatStagesByPlantId = (plantId) => axios.get(`${API_BASE_URL}/api/plants/${plantId}/flat-stages`, { headers: getAuthHeader() });
 
 // Growth Stage APIs
 const createGrowthStage = (data) => axios.post(`${API_BASE_URL}/api/growth-stages`, data, { headers: getAuthHeader() });
-const updateGrowthStage = (id, data) => axios.put(`/api/growth-stages/${id}`, data, { headers: getAuthHeader() });
-const deleteGrowthStage = (id) => axios.delete(`/api/growth-stages/${id}`, { headers: getAuthHeader() });
-const getGrowthStagesByPlantId = (plantId) => axios.get(`/api/growth-stages?plantId=${plantId}`, { headers: getAuthHeader() });
+const updateGrowthStage = (id, data) => axios.put(`${API_BASE_URL}/api/growth-stages/${id}`, data, { headers: getAuthHeader() });
+const deleteGrowthStage = (id) => axios.delete(`${API_BASE_URL}/api/growth-stages/${id}`, { headers: getAuthHeader() });
+const getGrowthStagesByPlantId = (plantId) => axios.get(`${API_BASE_URL}/api/growth-stages?plantId=${plantId}`, { headers: getAuthHeader() });
 
 export default {
     getCropsByField,
